ce38cfa2946d864e08ae1d933874dc53c7dfd88a
Treasure Porth <treasure.rose@gmail.com>
2014-12-22 14:36:54 -0800

    init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+                 $("#menu").click(function(){
+                     $("#body").load("menu.html");
+                 });
+            });
+
+$(document).ready(function(){
+                 $("#gallery").click(function(){
+                     $("#body").load("gallery.html");
+                 });
+            });
